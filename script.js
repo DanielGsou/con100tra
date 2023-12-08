9e907ff7b2e523f71c6088b295882f9378d1cd76
Daniel <94863402+itooko@users.noreply.github.com>
2023-12-08 19:51:22 -0300

    pequenas mudanças

--- script.js
+++ script.js
@@ -2,15 +2,19 @@ const html = document.querySelector('html')
 const focoButton = document.querySelector('.app__card-button--foco')
 const curtoButton = document.querySelector('.app__card-button--curto')
 const longoButton = document.querySelector('.app__card-button--longo')
+const banner = document.querySelector('.app__image')
 
 focoButton.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'foco')
+    banner.setAttribute('src', '/imagens/foco.png')
 })
 
 curtoButton.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-curto')
+    banner.setAttribute('src', '/imagens/descanso-curto.png')
 })
 
 longoButton.addEventListener('click', () => {
     html.setAttribute('data-contexto', 'descanso-longo')
+    banner.setAttribute('src', '/imagens/descanso-longo.png')
 })
