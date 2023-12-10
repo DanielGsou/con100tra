33d7f121238f07eeee393a8d100031a975472ebd
Daniel <94863402+itooko@users.noreply.github.com>
2023-12-10 03:59:03 -0300

    pequenas mudanças ²

--- script.js
+++ script.js
@@ -3,18 +3,49 @@ const focoButton = document.querySelector('.app__card-button--foco')
 const curtoButton = document.querySelector('.app__card-button--curto')
 const longoButton = document.querySelector('.app__card-button--longo')
 const banner = document.querySelector('.app__image')
+const titulo = document.querySelector('.app__title')
+const botoes = document.querySelectorAll('.app__card-button')
 
 focoButton.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco')
-    banner.setAttribute('src', '/imagens/foco.png')
+    alterarContexto('foco')
+    focoButton.classList.add('active')
 })
 
 curtoButton.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto')
-    banner.setAttribute('src', '/imagens/descanso-curto.png')
+    alterarContexto('descanso-curto')
+    curtoButton.classList.add('active')
 })
 
 longoButton.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo')
-    banner.setAttribute('src', '/imagens/descanso-longo.png')
+    alterarContexto('descanso-longo')
+    longoButton.classList.add('active')
 })
+
+function alterarContexto(contexto) {
+    botoes.forEach(function (contexto){
+        contexto.classList.remove("active")
+    })
+    html.setAttribute('data-contexto', contexto)
+    banner.setAttribute('src', `/imagens/${contexto}.png`)
+    switch (contexto) {
+        case "foco":
+        titulo.innerHTML = `
+        Otimize sua produtividade,<br>
+        <strong class="app__title-strong">mergulhe no que importa.</strong>
+        ` 
+            break;
+        case "descanso-curto":
+            titulo.innerHTML = `
+        Que tal uma respirada ?<br>
+        <strong class="app__title-strong">Faça uma pausa curta.</strong>
+        `
+            break;
+        case"descanso-longo":
+        titulo.innerHTML = `
+        Hora de voltar à superfície.<br>
+        <strong class="app__title-strong">Faça uma pausa longa.</strong>
+        `
+        default:
+            break;
+    }
+}
